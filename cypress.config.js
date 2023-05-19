305ef30d58cb6be422f0acce809902244b2c3b81
Lukasz Kawa <lukasz.kawa@telestream.net>
2023-05-19 23:32:46 +0200

    done

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,21 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    // baseUrl, etc
-    baseUrl: 'http://localhost:3000',
-    supportFile: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // and load any plugins that require the Node environment
-    },
-  },
+	e2e: {
+		// baseUrl, etc
+		baseUrl: 'http://localhost:3000',
+		supportFile: false,
+		env: {
+			users: {
+				standard: {
+					login: 'standard_user',
+					password: 'secret_sauce',
+				},
+			},
+		},
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+			// and load any plugins that require the Node environment
+		},
+	},
 })
--- cypress/e2e/logout2.cy.js
+++ cypress/e2e/logout2.cy.js
@@ -1,17 +1,16 @@
-// @ts-check
-
-// enables intelligent code completion for Cypress commands
-// https://on.cypress.io/intelligent-code-completion
-/// <reference types="cypress" />
-
 import { LoginPage } from './login.page'
 
-// enable the test once you get the username and the password
+// create a small type on the fly using jsdoc comment
+// just to help type check help us
+/** @type {{username: string, password: string}} */
+const user = Cypress.env('users').standard
+// we can even check if the user object is valid
+if (!user) {
+	throw new Error('Missing the standard user')
+}
+
 it('logs out', () => {
-	LoginPage.login(
-		Cypress.env('users').standard.login,
-		Cypress.env('users').standard.password
-	)
+	LoginPage.login(user.login, user.password)
 	cy.visit('/inventory.html')
 	cy.location('pathname').should('equal', '/inventory.html')
 	cy.contains('button', 'Open Menu').click()
