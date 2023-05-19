8a179396ffd88a4895faae02fbb0a111befd5e82
Lukasz Kawa <lukasz.kawa@telestream.net>
2023-05-19 16:03:30 +0200

    done

--- cypress/e2e/login.page.js
+++ cypress/e2e/login.page.js
@@ -2,31 +2,48 @@
 /// <reference types="cypress" />
 
 export const LoginPage = {
-  getUsername() {
-    return cy.get('[data-test="username"]')
-  },
-  getPassword() {
-    return cy.get('[data-test="password"]')
-  },
-  getError() {
-    return cy.get('[data-test=error]')
-  },
-  noErrors() {
-    cy.log('**there are no errors**')
-    LoginPage.getError().should('not.exist')
-    LoginPage.getUsername().should('not.have.class', 'error')
-    LoginPage.getPassword().should('not.have.class', 'error')
-  },
-  // new methods
-  getLogin() {
-    return cy.get('[data-test=login-button]')
-  },
-  showsError(text) {
-    cy.contains('[data-test=error]', text).should('be.visible')
-    LoginPage.getUsername().should('have.class', 'error')
-    LoginPage.getPassword().should('have.class', 'error')
-  },
-  login(username, password) {
-    // implement the user login using any of the previously shown methods
-  },
+	goTo() {
+		cy.visit('/')
+	},
+	getUsername() {
+		return cy.get('[data-test="username"]')
+	},
+	getPassword() {
+		return cy.get('[data-test="password"]')
+	},
+	getError() {
+		return cy.get('[data-test=error]')
+	},
+	noErrors() {
+		cy.log('**there are no errors**')
+		LoginPage.getError().should('not.exist')
+		LoginPage.getUsername().should('not.have.class', 'error')
+		LoginPage.getPassword().should('not.have.class', 'error')
+	},
+	// new methods
+	getLogin() {
+		return cy.get('[data-test=login-button]')
+	},
+	/**
+	 *
+	 * @param {string} text
+	 */
+	showsError(text) {
+		cy.contains('[data-test=error]', text).should('be.visible')
+		LoginPage.getUsername().should('have.class', 'error')
+		LoginPage.getPassword().should('have.class', 'error')
+	},
+	/**
+	 *
+	 * @param {string} username
+	 * @param {string} password
+	 */
+	login(username, password) {
+		cy.session(username + '-' + Date.now(), () => {
+			this.goTo()
+			this.getUsername().type(username)
+			this.getPassword().type(password)
+			this.getLogin().click()
+		})
+	},
 }
--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -7,19 +7,28 @@
 import { LoginPage } from './login.page'
 
 it('logs out', () => {
-  // use LoginPage to log the standard user in
-  // LoginPage.login(username, password)
-  // confirm we are logged in by visiting the inventory page
-  // https://on.cypress.io/visit
-  // https://on.cypress.io/location
-  // open the hamburger menu
-  // https://on.cypress.io/contains
-  // https://on.cypress.io/click
-  // the menu should appear
-  // in the menu find the "Logout" option and click on it
-  // we should be transported back to the index page "/"
-  // Confirm that we cannot go to the inventory page again
-  // and that the right error message is shown
-  // https://on.cypress.io/visit
-  // LoginPage.showsError
+	// use LoginPage to log the standard user in
+	LoginPage.login('standard_user', 'secret_sauce')
+	cy.visit('inventory.html')
+	cy.location('pathname').should('eq', '/inventory.html')
+	// confirm we are logged in by visiting the inventory page
+	// https://on.cypress.io/visit
+	// https://on.cypress.io/location
+	// open the hamburger menu
+	cy.get('#react-burger-menu-btn').click()
+	cy.get('.bm-item-list').find('#logout_sidebar_link').click()
+	// https://on.cypress.io/contains
+	// https://on.cypress.io/click
+	// the menu should appear
+	// in the menu find the "Logout" option and click on it
+	// we should be transported back to the index page "/"
+	cy.location('pathname').should('eq', '/')
+	cy.visit('inventory.html')
+
+	// Confirm that we cannot go to the inventory page again
+	// and that the right error message is shown
+	// https://on.cypress.io/visit
+	LoginPage.showsError(
+		`Epic sadface: You can only access '/inventory.html' when you are logged in.`
+	)
 })
